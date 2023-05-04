b6330e4d04b42de41f714d2bc6ac7340b8c5b157
Kazi Solayman <kazisolayman123@gmail.com>
2023-05-04 21:28:04 +0600

    recipe added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-      <p>helsdfsdlssdfsdfddo</p>
+      <p>heo</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
